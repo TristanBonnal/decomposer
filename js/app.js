bf9fcf4ff7990c101a499195b0b1d3c88c67e420
Tristan Bonnal <bonnal.tristan@hotmail.fr>
2022-01-09 15:01:50 +0100

    start cell, end cell and cursor

--- js/app.js
+++ js/app.js
@@ -1,62 +1,78 @@
 var app = {
-  init: function() {
-    console.log('init');
+    init: function() {
+        console.log('init');
 
-    // TODO
-    app.drawBoard();
+        // TODO
+        app.drawBoard();
 
-    // Event listeners - TODO
-  },
-  drawBoard: () => {
-    const boardElement = document.getElementById('board');
-    const rows = 4;
-    const columns = 6;
+        // Event listeners - TODO
+    },
 
-    for (let i = 1; i <= rows; i++) {
-        cellRowElement = document.createElement('div');
-        cellRowElement.classList.add('cellRow');
-        cellRowElement.id = 'row' + i;
-        for (j = 1; j <= columns; j++) {
-            cellElement = document.createElement('div');
-            cellElement.classList.add('cell');
-            cellRowElement.append(cellElement);
+    drawBoard: () => {
+        const boardElement = document.getElementById('board');
+        const rowsNb = 4;
+        const columnsNb = 6;
+
+        //Définition de la grille
+        for (let i = 1; i <= rowsNb; i++) {
+            cellRowElement = document.createElement('div');
+            cellRowElement.classList.add('cellRow');
+            cellRowElement.id = 'row' + i;
+            for (j = 1; j <= columnsNb; j++) {
+                cellElement = document.createElement('div');
+                cellElement.classList.add('cell');
+                cellRowElement.append(cellElement);
+            }
+            boardElement.append(cellRowElement);
         }
-        boardElement.append(cellRowElement);
+
+        //Définition cellule de départ et cellule d'arrivée
+        const firstRowElement = document.getElementById('row1');
+        const firstCellElement = firstRowElement.firstChild;
+        firstCellElement.classList.add('cellStart');
+
+        const lastRowElement = document.getElementById('row' + rowsNb);
+        const lastCellElement = lastRowElement.lastChild;
+        lastCellElement.classList.add('cellEnd');
+
+        //Définition position de départ du curseur
+        firstCellElement.classList.add('cellCurrent');
+
+
+    },
+    handleLaunchScriptButton: function() {
+        // TODO
+        
+        // TODO : get all lines as an array
+
+        window.setTimeout(function() {
+        app.codeLineLoop(codeLines, 0);
+        }, 2000);
+    },
+    codeLineLoop: function(codeLines, index) {
+        // Getting currentLine
+        var currentLine = codeLines[index];
+        console.log(currentLine);
+
+
+        // Increment
+        index++;
+
+        // if still a line to interpret
+        if (index < codeLines.length) {
+        // Recall same method (=> make a loop)
+        window.setTimeout(function() {
+            app.codeLineLoop(codeLines, index);
+        }, 1000);
+        } else {
+        window.setTimeout(function() {
+            app.checkSuccess();
+        }, 1000);
+        }
+    },
+    checkSuccess: function() {
+        // TODO display if the game is won or not
     }
-  },
-  handleLaunchScriptButton: function() {
-    // TODO
-    
-    // TODO : get all lines as an array
-
-    window.setTimeout(function() {
-      app.codeLineLoop(codeLines, 0);
-    }, 2000);
-  },
-  codeLineLoop: function(codeLines, index) {
-    // Getting currentLine
-    var currentLine = codeLines[index];
-    console.log(currentLine);
-
-
-    // Increment
-    index++;
-
-    // if still a line to interpret
-    if (index < codeLines.length) {
-      // Recall same method (=> make a loop)
-      window.setTimeout(function() {
-        app.codeLineLoop(codeLines, index);
-      }, 1000);
-    } else {
-      window.setTimeout(function() {
-        app.checkSuccess();
-      }, 1000);
-    }
-  },
-  checkSuccess: function() {
-    // TODO display if the game is won or not
-  }
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
